e1f459917ac489515f1a4cea891a6bcb11c6a0ee
Sandesh Kenchugundi <130632384+sandesh9160@users.noreply.github.com>
2025-06-12 20:55:14 +0530

    Delete vehicle_providers directory

--- vehicle_providers/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class VehicleProvidersConfig(AppConfig):
-    name = 'vehicle_providers'
--- vehicle_providers/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('dashboard/', views.vehicle_provider_dashboard, name='vehicle_provider_dashboard'),
-    path('add-ebike/', views.add_ebike, name='add_ebike'),
-    path('register-vehicle/', views.register_vehicle, name='register_vehicle'),
-]
-
