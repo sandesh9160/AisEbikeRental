74b612e81bb5c31d11402c18c98a72810728c873
Sandesh Kenchugundi <130632384+sandesh9160@users.noreply.github.com>
2025-06-14 23:29:59 +0530

    Update settings.py

--- ais_ebike_rental/settings.py
+++ ais_ebike_rental/settings.py
@@ -77,10 +77,11 @@ WSGI_APPLICATION = 'ais_ebike_rental.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
