f45ef55bdd2e416bd972ae8a68585e77441e9c19
Sandesh Kenchugundi <130632384+sandesh9160@users.noreply.github.com>
2025-06-15 23:26:53 +0530

    Update settings.py

--- ais_ebike_rental/settings.py
+++ ais_ebike_rental/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1xa3m_0xij=hrl71mww296skb-$%0&51dp8_tmku0s4g6tm69-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
