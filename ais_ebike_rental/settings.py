6fb70b3b653710df9eb5bc329f9f80784db15652
Sandesh Kenchugundi <130632384+sandesh9160@users.noreply.github.com>
2025-06-15 19:15:14 +0530

    Update settings.py

--- ais_ebike_rental/settings.py
+++ ais_ebike_rental/settings.py
@@ -124,10 +124,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR , 'static']
-import os
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
