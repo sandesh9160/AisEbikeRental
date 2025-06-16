d7f6bf3ddac1dc610ecd58286c0b1e1c859447cc
Sandesh Kenchugundi <130632384+sandesh9160@users.noreply.github.com>
2025-06-16 11:26:32 +0530

    Delete riders directory

--- riders/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('dashboard/', views.rider_dashboard, name='rider_dashboard'),
-    path('book/<int:ebike_id>/', views.book_ebike, name='book_ebike'),
-    path('booking/confirmation/<int:booking_id>/', views.booking_confirmation, name='booking_confirmation'),
-]
-
